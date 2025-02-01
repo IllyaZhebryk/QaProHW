bd9372572bde0e3acca1fb92ab72f51925b63e61
Illya Zhebryk <izhebrik@gmail.com>
2025-02-01 17:38:18 +0200

    виправлення розрахунку з від'ємном показником

--- js_hw3.js
+++ js_hw3.js
@@ -1,11 +1,13 @@
 function pow(x, y){
-let K = x;
-if (y < 0){
-    console.log("Показник ступеню не може бути менше 0. Введено: ");
-    return y;
+let K = x; 
+if (y < 0){y = -y;
+    for (let i = 1; i < y; i+=1){
+        K *= x;
+    }
+return 1/K;
     } else if (y == 0){
-    console.log("Результат дорівнює 1, бо показник зведення:");
-    return y;
+    const str = "Результат дорівнює 1"
+    return str;
 } else {    
     for (let i = 1; i < y; i+=1){
         K *= x;
@@ -13,4 +15,4 @@ if (y < 0){
 return K;   
 }
 };
-console.log(pow(3, 0));
+console.log(pow(3, -2));
