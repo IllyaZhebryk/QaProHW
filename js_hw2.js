7ee81a6c8bfa311da41ec9709c37913f3c084622
Illya Zhebryk <izhebrik@gmail.com>
2025-02-01 09:03:38 +0200

    the first

---
+++ js_hw2.js
@@ -0,0 +1,56 @@
+// 'number' + 3 + 3: string та дві конкатенації , number33
+//console.log('number' + 3 + 3);
+
+//null + 3 : null привело до числа та додало 3,  3
+//console.log(null + 3);
+
+// 5 && "qwerty" : не знаю
+//console.log(5 && "qwerty");// qwerty
+
+// +'40' + +'2' + "hillel"; привело перші стрінги в намбер, а потім стикнулось з контанценацією 
+//console.log(+'40' + +'2' + "hillel"); 42hillel
+
+// '10' - 5 === 6; стрінгу перевело в 10, мінус 5 - не дорівнює 6, фальш
+//console.log( '10' - 5 === 6); //false
+
+// true + false привело в 1+0
+//console.log(true + false);// 1
+
+// '4px' - 3 не змогло привести 4рх в цифру, тому NaN
+//console.log('4px' - 3);//NaN
+
+// '4' - 3 стрінгу перевело в цифру і мінус 3 = 1
+//console.log('4' - 3);// 1
+
+// '6' + 3 ** 0; 3 в ступені 0 це 1, а потім конкатенація, вийшло 61
+//console.log('6' + 3 ** 0);// 61
+
+// 12 / '6'
+//console.log( 12 / '6');// 2
+
+// '10' + (5 === 6);
+//console.log('10' + (5 === 6));//10false
+
+// null == ''
+//console.log(null == '');//false
+
+// 3 ** (9 / 3); 
+//console.log(3 ** (9 / 3));//27
+
+// !!'false' == !!'true' розібрали на занятті
+//console.log(!!'false' == !!'true');//true
+
+// 0 || '0' && 1
+//console.log(0 || '0' && 1);//1
+
+// (+null == false) < 1; зліва приблизно 1, справа 1, цифри рівні, фолс
+//console.log((+null == false) < 1);//false
+
+// false && true || true
+//console.log(false && true || true);//true
+
+// false && (false || true); не знаю
+//console.log(false && (false || true));//false4
+
+// (+null == false) < 1 ** 5; зліва приблизно 1, справа 1, цифри рівні, фолс
+//console.log((+null == false) < 1 ** 5);//false
